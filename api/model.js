63c61221d411a64a466df4506ec06019331bd2b4
davedwight <dpdwight@gmail.com>
2021-07-16 02:20:13 -0600

    result half shaped

--- api/model.js
+++ api/model.js
@@ -1,7 +1,61 @@
 const db = require("../data/db-config");
 
-function getRecipeById(id) {
-  return db("recipes as r").where("recipe_id", id);
+async function getRecipeById(id) {
+  const rows = await db("recipes as r")
+    .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
+    .leftJoin("step_ingredients as st", "s.step_id", "st.step_id")
+    .leftJoin("ingredients as i", "st.ingredient_id", "i.ingredient_id")
+    .select(
+      "r.recipe_name",
+      "s.step_number",
+      "s.step_instructions",
+      "i.ingredient_name",
+      "st.ingredient_qty"
+    )
+    .where("r.recipe_id", id)
+    .orderBy("s.step_number");
+
+  let result = {
+    recipe_name: rows[0].recipe_name,
+  };
+
+  rows.forEach((row) => {
+    if (row.step_number && !result.steps) {
+      result = {
+        ...result,
+        steps: [],
+      };
+      result.steps.push({
+        step_number: row.step_number,
+        step_instructions: row.step_instructions,
+      });
+    } else if (row.step_number && result.steps) {
+      result.steps.push({
+        step_number: row.step_number,
+        step_instructions: row.step_instructions,
+      });
+    }
+
+    // if (row.step_number && row.ingredient_name) {
+    //   result.steps.push({
+    //     step_number: row.step_number,
+    //     step_instructions: row.step_instructions,
+    //     ingredients: [],
+    //   });
+    //   rows.forEach((row) => {
+    //     result.steps.ingredients.push({
+    //       ingredient_name: row.ingredient_name,
+    //       ingredient_qty: row.ingredient_qty,
+    //     });
+    //   });
+    // } else if (row.step_number) {
+    //   result.steps.push({
+    //     step_number: row.step_number,
+    //     step_instructions: row.step_instructions,
+    //   });
+    // }
+  });
+  return result;
 }
 
 module.exports = {
